df46c1473b1b25cc715f9c644e28317243d71c5a
Letsunch <131334733+Letsunch@users.noreply.github.com>
2024-05-09 11:50:10 +0200

    Add files via upload

---
+++ DataLayer/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DataAccessLayer
+{
+    public class DataLayer
+    {
+        private string connectionString = "Data Source=DESKTOP-URR6DQA\\SQLEXPRESS;Initial Catalog=CALCULATED;Integrated Security=True;";
+
+        public void SaveCalculation(string expression)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "INSERT INTO Calculations (Expression) VALUES (@Expression);";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@Expression", expression);
+
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+        }
+
+    }
+}
